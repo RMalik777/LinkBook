9a90412bb46775c9a794ca14b42407999e17a556
Rafli Malik <Rafli.Malik@gmail.com>
2024-05-13 10:38:28 +0700

    feat: add color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{html,js,svelte,ts}"],
+  darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				"main-light": "url('./src/lib/assets/bglight.svg')",
+				"main-dark": "url('./src/lib/assets/bgdark.svg')"
+			},
+			colors: {
+				theme: {
+					black: "#222222",
+					white: "#F5F5F5",
+					purple: "#6E3FBD",
+					green: "#01A9BE",
+					yellow: "#FEB102",
+					orange: "#FB580F"
+				}
+			}
+		}
 	},
-	plugins: [],
+	plugins: []
 };
