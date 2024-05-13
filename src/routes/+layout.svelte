d20e82cb57728c1cdabd087d17990cd41ab7c2bc
Rafli Malik <Rafli.Malik@gmail.com>
2024-05-13 15:07:55 +0700

    fix: remove multiple unused font import

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,7 +17,7 @@
 		rel="stylesheet"
 		href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 	<link
-		href="https://fonts.googleapis.com/css2?family=Bellota+Text:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Bellota:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Inter:wght@100..900&family=Jersey+10+Charted&display=swap"
+		href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
 		rel="stylesheet" />
 </svelte:head>
 
