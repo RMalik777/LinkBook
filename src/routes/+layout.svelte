d528c93a3c673c7eef6fb92965369dfeb8f8dfa6
Rafli Malik <Rafli.Malik@gmail.com>
2024-05-13 10:40:33 +0700

    fix: fix title and description

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
 	import { dev } from "$app/environment";
 	import { inject } from "@vercel/analytics";
+	import "../app.css";
 
 	inject({ mode: dev ? "development" : "production" });
 </script>
 
 <svelte:head>
-	<title>Rafli | Link Book</title>
+	<title>Link Book | Rafli Malik</title>
 	<meta
 		name="description"
-		content="Rafli Malik link dictionary for social media, website and portofolio" />
+		content="Rafli Malik link dictionary for his social media, website and portofolio" />
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 	<link
 		href="https://fonts.googleapis.com/css2?family=Bellota+Text:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Bellota:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Inter:wght@100..900&family=Jersey+10+Charted&display=swap"
 		rel="stylesheet" />
