6b86977097f2f5ea3480a43cb667364d97749783
Rafli Malik <Rafli.Malik@gmail.com>
2024-05-13 10:38:18 +0700

    feat: add new link data

--- src/lib/LinkData.js
+++ src/lib/LinkData.js
@@ -1,32 +1,44 @@
 export const LinkData = [
 	{
-		title: "Personal Website",
+		icon: "tab",
+		title: "Website",
 		desc: "www.raflimalik.com",
-    url: "https://www.raflimalik.com",
+		url: "https://www.raflimalik.com"
 	},
 	{
-		title: "Personal Blog Website",
-		desc: "Coming Soon",
-    url: "",
+		icon: "tab",
+		title: "Blog Website (Coming Soon)",
+		desc: "blog.raflimalik.com",
+		url: "https://blog.raflimalik.com"
 	},
 	{
+		icon: "folder_data",
+		title: "Github",
+		desc: "RMalik777",
+		url: "https://github.com/RMalik777"
+	},
+	{
+		icon: "mail",
 		title: "Personal Email",
 		desc: "Rafli.Malik@gmail.com",
-		url: "mailto:Rafli.Malik@gmail.com",
+		url: "mailto:Rafli.Malik@gmail.com"
 	},
 	{
+		icon: "mail",
 		title: "University Email",
 		desc: "Rafli.Malik@binus.ac.id",
-		url: "mailto:Rafli.Malik@binus.ac.id",
+		url: "mailto:Rafli.Malik@binus.ac.id"
 	},
 	{
+		icon: "groups",
 		title: "LinkedIn",
 		desc: "Rafli Malik",
-		url: "https://www.linkedin.com/in/rafli-malik/",
+		url: "https://www.linkedin.com/in/rafli-malik/"
 	},
 	{
+		icon: "chat",
 		title: "Discord",
 		desc: "play7",
-		url: "https://discord.com/users/432877551640117249",
-	},
+		url: "https://discord.com/users/432877551640117249"
+	}
 ];
