e6f156c1f52a70958e50e5ea37e2b5700c868e40
Rafli Malik <Rafli.Malik@gmail.com>
2024-08-26 23:25:05 +0700

    style: dark mode animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 
   import { LinkData } from "$lib/LinkData";
 
-  import { Sun, Moon } from "lucide-svelte";
+  import { Sun, Moon, SunMoon } from "lucide-svelte";
 
   import portrait from "$lib/assets/photosquarezoom.webp";
   import githubBlack from "$lib/assets/githubmarkblack.png";
@@ -44,7 +44,7 @@
   class="min-w-dvw flex h-fit min-h-dvh w-full max-w-full flex-col items-center justify-between gap-8 scroll-smooth bg-main-light bg-cover bg-center pt-4 antialiased duration-200 ease-out dark:bg-main-dark md:gap-12">
   <button
     type="button"
-    class="group mr-4 flex h-fit items-center gap-0 self-end rounded-lg bg-orange-200 px-2 py-1.5 text-sm font-medium text-neutral-900 duration-200 ease-out hover:bg-orange-300 dark:bg-orange-800 dark:text-neutral-50 dark:hover:bg-orange-700 sm:px-4 sm:py-2 md:gap-1 md:px-5 md:py-3 md:text-base"
+    class="group mr-4 flex h-fit items-center gap-0 self-end rounded-lg bg-orange-200 px-2 py-1.5 text-sm font-medium text-neutral-900 duration-200 ease-out hover:bg-orange-300 hover:shadow-md focus:bg-orange-300 focus:shadow-md dark:bg-orange-800 dark:text-neutral-50 dark:hover:bg-orange-700 sm:px-4 sm:py-2 md:gap-1 md:px-5 md:py-3 md:text-base"
     on:click={() => {
       if (browser) {
         // if the theme is dark, change to light
@@ -73,12 +73,16 @@
         }
       }
     }}>
-    {#if themeMode === "Dark"}
-      <Moon class={"h-4 sm:h-5 md:h-6"} />
-    {:else}
-      <Sun class={"h-4 sm:h-5 md:h-6"} />
-    {/if}
-    {themeMode ? themeMode : "System"}</button>
+    <Moon
+      class={(themeMode === "Dark" ? "rotate-0 scale-100" : "rotate-90 scale-0") +
+        " relative h-4 duration-300 ease-out sm:h-5 md:h-6"} />
+    <Sun
+      class={(themeMode === "Light" ? "rotate-0 scale-100" : "rotate-90 scale-0") +
+        " absolute h-4 duration-300 ease-out sm:h-5 md:h-6"} />
+    <SunMoon
+      class={(themeMode !== "Light" && themeMode !== "Dark" ? "rotate-0 scale-100" : "-rotate-90 scale-0") +
+        " absolute h-4 duration-300 ease-out sm:h-5 md:h-6"} />
+    <span class="duration-200 ease-out">{themeMode ? themeMode : "System"}</span></button>
   <header class="flex flex-col items-center justify-center gap-1">
     <img src={portrait} alt="Portrait of Rafli Malik" class="size-1/5 rounded-full sm:size-1/6" />
     <h1
@@ -90,7 +94,7 @@
   <main class="flex w-dvw min-w-fit max-w-full flex-col items-center gap-4 rounded-xl px-4 sm:px-0">
     {#each LinkData as link}
       <a
-        class="sm:text-md group flex w-full flex-col rounded-lg bg-purple-100 px-4 py-2 text-sm shadow-md duration-200 ease-out hover:bg-purple-200 hover:shadow-lg dark:bg-purple-900 dark:text-gray-200 dark:hover:bg-purple-800 sm:w-1/2 lg:w-1/3"
+        class="sm:text-md group flex w-full flex-col rounded-lg bg-purple-100 px-4 py-2 text-sm shadow-md duration-200 ease-out hover:bg-purple-200 hover:shadow-lg focus:bg-purple-200 focus:shadow-lg dark:bg-purple-900 dark:text-gray-200 dark:hover:bg-purple-800 dark:focus:bg-purple-800 sm:w-1/2 lg:w-1/3"
         href={link.url}
         target="_blank">
         <div class="flex flex-row items-start justify-start gap-2">
@@ -115,14 +119,15 @@
         target="_blank"
         rel="noopener noreferrer"
         href="https://bgjar.com"
-        class="text-blue-500 duration-200 ease-out after:content-['_↗'] hover:underline dark:text-blue-300">BGJar</a>
+        class="text-blue-500 duration-200 ease-out after:content-['_↗'] hover:underline focus:underline dark:text-blue-300"
+        >BGJar</a>
     </p>
     <p class="flex flex-row items-center gap-2">
       Source available in <a
         href="https://github.com/RMalik777/LinkBook"
         target="_blank"
         rel="noopener noreferrer"
-        class="flex items-center gap-1 text-blue-500 duration-200 ease-out after:content-['_↗'] hover:underline dark:text-blue-300"
+        class="flex items-center gap-1 text-blue-500 duration-200 ease-out after:content-['_↗'] hover:underline focus:underline dark:text-blue-300"
         ><img
           loading="lazy"
           decoding="async"
@@ -130,7 +135,10 @@
           alt=""
           class="h-5" />GitHub</a>
     </p>
-    <a translate="no" href="https://www.raflimalik.com/" target="_blank" class="text-theme-green hover:underline"
-      >&copy; 2024 Rafli Malik</a>
+    <a
+      translate="no"
+      href="https://www.raflimalik.com/"
+      target="_blank"
+      class="text-theme-green hover:underline focus:underline">&copy; 2024 Rafli Malik</a>
   </footer>
 </div>
